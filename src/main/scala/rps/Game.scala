c9e6b0964d8fb46a86dc83b87a9a1e0a5923e515
Matteo Turri <matteo.turri@buildo.io>
2017-12-12 12:57:33 +0100

    Step 5

--- src/main/scala/rps/Game.scala
+++ src/main/scala/rps/Game.scala
@@ -1,37 +1,24 @@
 package rps
 
 import scala.util.Random
-import io.buildo.enumero.CaseEnumIndex
+import model.{ Move, Result, Response}
 import io.buildo.enumero.CaseEnumSerialization
 
 object Game {
-  def play(): Unit = {
-    println("Please select your move: input 0 for Rock, 1 for Paper and 2 for Scissors")
-    val userInput: String = scala.io.StdIn.readLine();
-    val userMove: Option[Move] = matchInput(userInput)
+  def play(userMove: Move): Response = {
     val cpuMove: Move = getCpuMove()
-    userMove.map(x => getOutcome(x, cpuMove)).getOrElse(println("User input is not valid"))
-  }
- 
-  private def matchInput(x: String): Option[Move] = {
-    CaseEnumIndex[Move].caseFromIndex(x)
-  }
-
-  private def getMoveString(x: Move): String = {
-    CaseEnumSerialization[Move].caseToString(x)
+    Response(userMove, cpuMove, getOutcome(userMove, cpuMove))
   }
 
   private def getCpuMove(): Move = {
     Random.shuffle(CaseEnumSerialization[Move].values).head
   }
 
-  private def getOutcome(user: Move, cpu: Move): Unit = {
-    println("User move is "+getMoveString(user))
-    println("CPU move is "+getMoveString(cpu))
+  private def getOutcome(user: Move, cpu: Move): Result = {
     (user,cpu) match {
-      case (Move.Rock, Move.Scissors) | (Move.Paper, Move.Rock) | (Move.Scissors, Move.Paper) => println("User wins!")
-      case (x, y) if x == y => println("Draw")
-      case _ => println("CPU wins!")
+      case (Move.Rock, Move.Scissors) | (Move.Paper, Move.Rock) | (Move.Scissors, Move.Paper) => Result.Win
+      case (x, y) if x == y => Result.Draw
+      case _ => Result.Lose
     }
   }
 }
